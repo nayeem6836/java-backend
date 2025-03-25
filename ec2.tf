3edd1dca8b6c91bd4329fd0632f07d68db9a898d
nayeem6836 <maseednayeem@gmail.com>
2025-03-25 22:12:36 +0530

    new files added

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_ecs_service" "example" {
+  name    = "example"
+  cluster = aws_ecs_cluster.example.id
+
+  alarms {
+    enable   = true
+    rollback = true
+    alarm_names = [
+      aws_cloudwatch_metric_alarm.example.alarm_name
+    ]
+  }
+}
